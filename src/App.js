7af829c90c45268bc4abb4443db5ba2caab6f8cd
km00776 <kmurugathas@gmail.com>
2020-11-01 01:19:22 +0000

    AI with News react project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,  useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
+import wordsToNumbers from 'words-to-numbers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import useStyles from './styles.js';
+
+const alanKey = '3d16b7cb1cde46042219c2286ed992b02e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+// parent component
+const App = () => {
+    const[newsArticles, setNewsArticles] = useState([]);
+    const[activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles();
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command, articles, number}) => {
+                if(command === 'newHeadlines') {
+                   setNewsArticles(articles);
+                   setActiveArticle(-1);
+                }
+                else if(command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                }
+                else if(command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy: true}) : number;
+
+                    const article = articles[parsedNumber - 1];
+
+                    if(parsedNumber > 20) {
+                        alanBtn().playText('Please try that again.');
+                    }
+                    else if(article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...')
+                    }
+                }
+            }
+        })  
+    }, [])
+
+
+    return(
+        <div>
+            <div className={classes.logoContainer}>
+                <img src=" https://s.tmimgcdn.com/scr/800x500/78500/robot-logo-template_78572-2-original.jpg" className={classes.alanLogo} alt="alan logo"/>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+        </div>
+    )
 }
 
 export default App;
